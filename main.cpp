45ff5e6f8b4a18437e1bb99d3ad2cd5b15237f23
Piotr-Nowak01 <62243821+Piotr-Nowak01@users.noreply.github.com>
2020-03-23 15:52:15 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,22 +13,22 @@ int main() {
 	int suma=0;			
 	int srednia;		// œredni czas dzia³ania algorytmu
 	double x=1;			// ile % pocz¹tkowych indeksów tablicy ma byæ posortowane
-	for (int k=0; k<100; k++)
+	for (int k=0; k<1; k++)
 	{
 		int *T = new int [n];
 		for (int i=0; i<n;i++)
 			{
-//				T[i]=rand()%n;
+				T[i]=rand()%n;
 //				cout<<T[i]<<" ";
 //			}
 //		for (int i=x*n; i<n;i++)
 //			{
-				T[i]=n-i;
+//				T[i]=n-i;
 //				cout<<T[i]<<" ";
 			}
 		cout<<endl;	
 		auto t1=chrono::high_resolution_clock::now();
-		introsort(T,p,r);  //sortowanie tablicy
+		sort_scalanie(T,p,r);  //sortowanie tablicy
 		auto t2=chrono::high_resolution_clock::now();
 		auto duration= chrono::duration_cast<chrono::microseconds>(t2-t1).count();
 		for(int i=0;i<n;i++)
--- scalanie.cpp
+++ scalanie.cpp
@@ -1,45 +1,46 @@
 #include "scalanie.h"
 void scalanie(int T[], int p, int q, int r)
 {
-int *pom = new int [r+1];
-int i=p,j=q+1;
-for(int i=p;i<=r; i++) 
-    {
-		pom[i] = T[i];
-	}
-for(int k=p;k<=r;k++) 
+	int *pom = new int [r-p+1];
+	int i=p,j=q+1;
+	int k=0;
+	while (i<=q && j<=r)
 	{
-		if(i<=q && j <= r)
-	    {
-	    	if (pom[i]<=pom[j])
-	    	{
-	    		T[k]=pom[i];
-	    		i++;
-			}
-			else
-			{
-				T[k]=pom[j];
-				j++;
-			}
-		}
-		else if (i<=q)
+		if (T[i]<=T[j])
 		{
-			T[k]=pom[i];
+			pom[k]=T[i];
 			i++;
 		}
-		else 
+		else
 		{
-			T[k]=pom[j];
+			pom[k]=T[j];
 			j++;
 		}
+		k++;
 	}
-    delete [] pom;
+	while (i<=q)
+	{
+		pom[k]=T[i];
+		i++;
+		k++;
+	}
+	while (j<=r)
+	{
+		pom[k]=T[j];
+		j++;
+		k++;
+	}
+	for(int i=p;i<=r;i++)
+	{
+		T[i]=pom[i-p];
+	}
+	delete [] pom;
 }
 void sort_scalanie(int T[], int p, int r)
 {
 	if (p<r)
 	{
-		int	q=(p+r)/2; //srodek przedzia³u
+		int	q=p+(r-p)/2; //srodek przedzia³u
 		sort_scalanie(T,p,q); 
 		sort_scalanie(T,q+1,r);
 		scalanie(T,p,q,r);
